66dcabfc38eb0f34f18be1b4f47cc3c6e059bf09
habanero2012 <sato1101@live.jp>
2021-08-22 17:02:10 +0900

    add file

--- bin/exec.rb
+++ bin/exec.rb
@@ -1,3 +1,14 @@
 require 'newrelic_rpm'
 
 NewRelic::Agent.manual_start
+
+puts 'start'
+
+duration = rand(1..3)
+puts "duration is #{duration}"
+$stdout.flush
+
+sleep duration
+raise 'unlucky error' if duration == 1
+
+puts 'end'
